679e412ee35331ebc9c6dd03509c9cdd5bf9105c
Hamilton Little <hamilton.little@gmail.com>
2013-07-06 12:53:08 -0700

    Modify home page styling. Spec navbar links on app pages.

---
+++ spec/features/application_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Application Pages" do
+
+  before { visit root_path }
+
+  subject { page }
+
+  describe "navbar" do
+
+    it "should have the right links" do
+      page.should have_link("Cal Poly Men's Water Polo", href: root_path)
+      page.should have_link("Schedule", href: '#')
+      page.should have_link("Roster", href: '#')
+      page.should have_link("Administrativa", href: '#')
+    end
+  end
+end
--- spec/features/post_pages_spec.rb
+++ spec/features/post_pages_spec.rb
@@ -4,10 +4,23 @@ describe "Post pages" do
 
   subject { page }
 
-  describe "Home page" do
+  describe "#show (Home Page)" do
     before { visit root_path }
 
     it { should show_home_page }
+
+    describe "new user sidebar" do
+
+      it "should get the attention of a newcomer" do
+        page.should have_content("New Here?")
+      end
+
+      it "should have the right links" do
+        page.should have_link("Join the Team", href: '#')
+        page.should have_link("Contact an Officer", href: '#')
+        page.should have_link("View Season Schedule", href: '#')
+      end
+    end
 
     describe "Announcements" do
       let!(:post1) { FactoryGirl.create(:post, content: "This is post 1") }
@@ -16,23 +29,26 @@ describe "Post pages" do
       before { visit root_path }
 
       it "should show all of the announcements" do
-        Post.all.each do |post|
-          ap post
-        end
-        expect(page).to have_selector('li.post', text: post1.content)
-        expect(page).to have_selector('li.post', text: post2.content)
-        expect(page).to have_selector('li.post', text: post3.content)
+        expect(page).to have_selector('li.content', text: post1.content)
+        expect(page).to have_selector('li.content', text: post2.content)
+        expect(page).to have_selector('li.content', text: post3.content)
 
-        expect(page).to have_selector('p.pull-right', text: post1.author.name)
-        expect(page).to have_selector('p.pull-right', text: post2.author.name)
-        expect(page).to have_selector('p.pull-right', text: post3.author.name)
+        expect(page).to have_selector('small.meta-data',
+                                      text: post1.author.name)
+        expect(page).to have_selector('small.meta-data',
+                                      text: post2.author.name)
+        expect(page).to have_selector('small.meta-data',
+                                      text: post3.author.name)
 
-        expect(page).to have_selector('p.pull-right',
-                                      text: time_in_words(post1.created_at))
-        expect(page).to have_selector('p.pull-right',
-                                      text: time_in_words(post2.created_at))
-        expect(page).to have_selector('p.pull-right',
-                                      text: time_in_words(post3.created_at))
+        expect(page).to have_selector('small.meta-data', text:
+                                      post1.updated_at.strftime(
+                                        "posted: %B %d, %Y %H:%M"))
+        expect(page).to have_selector('small.meta-data',
+                                      post2.updated_at.strftime(
+                                        "posted: %B %d, %Y %H:%M"))
+        expect(page).to have_selector('small.meta-data',
+                                      post3.updated_at.strftime(
+                                        "posted: %B %d, %Y %H:%M"))
       end
     end
   end
